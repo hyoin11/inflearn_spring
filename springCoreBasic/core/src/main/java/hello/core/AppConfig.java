6afc955b87db91276fa02058b1427bd543bdbf96
hyoin <kslove1111@gmail.com>
2023-05-22 19:09:29 +0900

    스프링 핵심 원리 이해2-객체 지향 원리 적용_AppConfig 리팩터링

--- springCoreBasic/core/src/main/java/hello/core/AppConfig.java
+++ springCoreBasic/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,11 +10,26 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {    // application 전체를 설정하고 구성
 
+    /*
+        메서드 명을 보면 역할이 드러남
+        구현체도 어떤걸 사용하는지 확인할 수 있음
+        설계에 대한 그림이 구성정보에 그대로 드러남
+        역할과 구현 클래스가 한눈에 들어옴. 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있음
+     */
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
