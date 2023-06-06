a6ac6d410c97b8d66eac086f951f2fbb9723e0a2
hyoin <kslove1111@gmail.com>
2023-06-06 21:01:51 +0900

    섹션5.싱글톤 컨테이너_@Configuration과 바이트코드 조작의 마법

--- springCoreBasic/core/src/main/java/hello/core/AppConfig.java
+++ springCoreBasic/core/src/main/java/hello/core/AppConfig.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
