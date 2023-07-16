f5369767b24df96ca42016cb04bd3c3c205e6c38
hyoin <kslove1111@gmail.com>
2023-07-16 20:02:02 +0900

    섹션8.빈 생명주기 콜백_빈 생명주기 콜백 시작

---
+++ springCoreBasic/core/src/test/java/hello/core/lifeCycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.core.lifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class); // ApplicationContext 는 close()를 제공하지 않음. 그래서 하위 ConfigurableApplicationContext로 생성
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
